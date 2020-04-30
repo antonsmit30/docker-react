ab4a845cc39783cb4bd34032d36ac90fabdf01b4
Anton <anton.smit@three6five.com>
2020-04-30 11:37:51 +0200

    fixed errors

--- src/App.js
+++ src/App.js
@@ -2,98 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-
-  render() {
-    return (
-      <button 
-      className="square" 
-      onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
 function App() {
   return (
     <div className="App">
-      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>TIC TAC TOE!
-        <Game/>
-        </h1>
         <p>
-          This web app is running inside a docker container!<br></br>
-          pretty neat right!
+          This is the updated react app.
         </p>
-        {/* <a
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
